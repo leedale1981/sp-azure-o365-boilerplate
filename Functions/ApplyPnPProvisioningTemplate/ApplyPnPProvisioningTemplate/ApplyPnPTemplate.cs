aeb6cfa61bdc6a353ebad401611b7044dfcc322c
Lee Dale <lee.jdale@gmail.com>
2018-02-05 14:48:42 +0000

    Updates

--- Functions/ApplyPnPProvisioningTemplate/ApplyPnPProvisioningTemplate/ApplyPnPTemplate.cs
+++ Functions/ApplyPnPProvisioningTemplate/ApplyPnPProvisioningTemplate/ApplyPnPTemplate.cs
@@ -1,17 +1,33 @@
-using System;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
+using Microsoft.SharePoint.Client;
+using OfficeDevPnP.Core;
+using OfficeDevPnP.Core.Framework.Provisioning.Model;
+using OfficeDevPnP.Core.Framework.Provisioning.Providers.Xml;
+using System;
 
 namespace ApplyPnPProvisioningTemplate
 {
     public static class ApplyPnPTemplate
     {
         [FunctionName("ApplyPnPTemplate")]
-        public static void Run([QueueTrigger("pnpprovisioningqueue", Connection = "AzureQueueConnection")]string myQueueItem, TraceWriter log)
+        public static void Run([QueueTrigger("pnpprovisioningqueue", Connection = "AzureQueueConnection")]string siteUrl, TraceWriter log)
         {
-            log.Info($"C# Queue trigger function processed: {myQueueItem}");
+            log.Info($"Azure Function Queue Item - Applying PnP Template to site: {siteUrl}");
 
-            Environment.
+            string appId = Environment.GetEnvironmentVariable("AppClientID");
+            string appSecret = Environment.GetEnvironmentVariable("AppClientSecret");
+
+            AuthenticationManager authManager = new AuthenticationManager();
+
+            using (var clientContext = authManager.GetAppOnlyAuthenticatedContext(siteUrl, appId, appSecret))
+            {
+                string templatePath = @"D:\home\site\wwwroot\ApplyPnPTemplate\";
+                XMLFileSystemTemplateProvider provider = new XMLFileSystemTemplateProvider(templatePath, "");
+                ProvisioningTemplate template = provider.GetTemplate("FlowDemoTemplate.xml");
+
+                clientContext.Web.ApplyProvisioningTemplate(template);
+            }
         }
     }
 }
--- SiteDesign/DeploySiteScript.ps1
+++ SiteDesign/DeploySiteScript.ps1
@@ -1,11 +1,18 @@
+$programFiles = [environment]::getfolderpath("programfiles")
+
+Add-Type -Path "C:\Program Files\SharePoint Online Management Shell\Microsoft.Online.SharePoint.PowerShell\Microsoft.SharePoint.Client.dll"
+
+$username = "leedale@att-office.info"
+$password = "M1llwall03"
+$cred = New-Object -TypeName System.Management.Automation.PSCredential -argumentlist $username, $(convertto-securestring $password -asplaintext -force)
+Connect-SPOService -Url https://leedale8850hotmail-admin.sharepoint.com -Credential $cred
+
 
 # Get the site script JSON from file.
 
-Get-Content 'ld-test-script.json' `
-     -Raw | `
-     Add-SPOSiteScript `
-    -Title "LD Demo Script"
+$content = Get-Content 'C:\src\SP-Azure-O365-Boilerplate\SiteDesign\ld-test-script.json' -Raw
 
+Add-SPOSiteScript -Title "LD Demo PnP Script" -Content $content
 
 
 # Add site design and assoctiate script
@@ -13,5 +20,7 @@ Get-Content 'ld-test-script.json' `
 Add-SPOSiteDesign `
   -Title "LD Demo" `
   -WebTemplate "64" `
-  -SiteScripts "2756067f-d818-4933-a514-2a2b2c50fb06" `
+  -SiteScripts "7623d64f-2f03-4ad9-aeb9-1758ab9e53ca" `
   -Description "LD Demo"
+
+Get-SPOSiteScript
